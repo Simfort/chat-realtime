5738c048b6446cbb034c88b15405edc8166a1702
Simfort <lemondzavadavid265@gmail.com>
2025-07-02 20:56:54 +0300

    over project

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { Response } from "express";
 import jwt from "jsonwebtoken";
-
+import * as db from "./db.json" with {type:"json"};
+import * as messages from "./messages.json" with {type:"json"};
 export const generateToken = (userId: number, res: Response) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET as string, {
     expiresIn: "7d",
